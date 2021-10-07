f3c49efe694b724b5e1a25c8eafd00edc8bfc926
hugoofilipe <filipeeduarte@gmail.com>
2021-10-07 21:34:16 +0100

    add contacrt form testing

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,9 +2,11 @@
     <div>olaola Returned Id: {{mailResponse}}
         <form @submit.prevent="submit">
             <input type="text" v-model="name">
-                <button type="submit">
-                    Submit From Vue Property
-                </button>
+            <input type="text" v-model="tel">
+            <input type="text" v-model="text">
+            <button type="submit">
+                Submit From Vue Property
+            </button>
         </form>
     </div>
 
@@ -21,15 +23,21 @@ export default {
   name: 'contactForm',
   data () {
     return {
-      email: null,
       name: null,
-      mailResponse: null
+      tel: null,
+      text: null
     }
   },
   methods: {
     submit () {
-      console.log('sending')
-      const mail = { to: 'geral@huna.pt' }
+      console.log('Sending')
+      const mail = {
+        to: 'geral@huna.pt',
+        subject: 'Contact form from website HUNA',
+        name: this.name,
+        tel: this.tel,
+        text: this.text
+      }
       // Simple POST request with a JSON body using axios
       axios.post('https://huna.pt/api/text-mail', mail)
     }
