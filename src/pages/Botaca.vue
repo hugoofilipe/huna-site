3949840f97d58110bf2fb8e595621f5854cf51b0
hugoofilipe <filipeeduarte@gmail.com>
2024-03-21 16:33:56 +0000

    new report for botaca

--- src/pages/Botaca.vue
+++ src/pages/Botaca.vue
@@ -19,7 +19,8 @@
         no-caps
       /> -->
       <iframe width="1024" height="768" src="https://lookerstudio.google.com/embed/reporting/cd6c6d08-8dcd-4a49-9e6d-81cf6b325508/page/6zXD" frameborder="0" style="border:0" allowfullscreen></iframe>
-
+      <iframe width="1024" height="769" src="https://lookerstudio.google.com/embed/reporting/4b14f6f6-cb12-4ae4-a685-3850b5c8fc98/page/tWDGB" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+      <iframe width="1024" height="768" src="https://lookerstudio.google.com/embed/reporting/284a340a-f52a-45a5-89ad-9e72fd4020cb/page/4VDGB" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
     </div>
   </div>
 </template>
